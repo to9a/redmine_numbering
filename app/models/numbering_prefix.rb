1353cdf1ff626ccd9fbe9915c621e9426ee82dbc
to9a <to9a0111@gmail.com>
2014-08-12 17:15:57 +0900

    不要なモデルを削除

--- app/models/numbering_prefix.rb
+++
@@ -1,9 +0,0 @@
-class NumberingPrefix < ActiveRecord::Base
-  unloadable
-
-  validates_presence_of :fixed
-  validates_length_of :fixed, :maximum => 255
-  validates_length_of :subject, :maximum => 255
-
-  validates_length_of :document, :maximum => 255
-end
